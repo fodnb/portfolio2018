99c009edeba9c8bb2bb7d18961aa2077627acda5
fodnb <Daniel.Mcghee6813@gmail.com>
2018-09-18 11:47:31 -0400

    Add background image

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Header from './Header';
 
 const DashboardPage = () => (
   <div>
+    <Header />
     Dashboard page content
   </div>
 );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Router, Route, Switch} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/DashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
-import LoginPage from '../components/LoginPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import Header from '../components/Header';
 
 export const history = createHistory();
 
@@ -13,8 +11,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/" component={LoginPage} exact={true} />
-        <PrivateRoute path="/dashboard" component={DashboardPage} />
+        <Route path="/dashboard" component={DashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
